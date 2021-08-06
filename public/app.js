e57c3e56dd19526528b94ec7bfceb3304f84b7ff
br j <honuj9983@gmail.com>
2021-08-06 21:05:25 +0900

    Add  front static files

--- public/app.js
+++ public/app.js
@@ -1,1 +1,18 @@
-console.log('front');
+const socket = io('http://localhost:3000');
+
+socket.on('connect', () => {
+  console.log('Connected to Server ✅');
+  socket.emit('message', '안녕하세요');
+});
+socket.on('message', (data) => {
+  console.log('message', data);
+});
+socket.on('events', (data) => {
+  console.log('event', data);
+});
+socket.on('exception', (data) => {
+  console.log('event', data);
+});
+socket.on('disconnect', () => {
+  console.log('Disconnected from Server ❌');
+});
